9600fe3d6ba5d04432d42032fa33b8f7c43cc60b
hiteshgallopbiz <hitesh.gallopbiz@gmail.com>
2023-04-14 15:40:55 +0530

    upoate

--- calc/calc_test.go
+++ calc/calc_test.go
@@ -34,3 +34,27 @@ func TestDivision(t *testing.T) {
 		t.Errorf("%d was expect but got %d .\n", exp, res)
 	}
 }
+
+func BenchmarkAddition(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		Addition(1, 2)
+	}
+}
+
+func BenchmarkSubstraction(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		Substraction(1, 2)
+	}
+}
+
+func BenchmarkMultiplication(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		Multiplication(1, 2)
+	}
+}
+
+func BenchmarkDivision(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		Division(9, 3)
+	}
+}
